549962a7227a1ddcfc2156afc2e376f4a26124d6
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-08 12:32:13 +0000

    [Add]3-15課題2:index.showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all #Bookのデータ全部
     @book = Book.new #@bookはindex.htmlとつなぐインスタンス変数
   end
 
@@ -10,9 +11,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/books' => 'books#index'  #新規投稿と投稿一覧表はここ
   post '/books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book' #投稿詳細はここ
-  get 'books/edit' #投稿編集はここ
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book' #投稿編集はここ
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
