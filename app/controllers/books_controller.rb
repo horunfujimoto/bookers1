7638ba238fab1a8a03790550617b0a85af3161cc
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-09 04:39:19 +0000

    [Add]3-15課題2:editページの作成、flashメッセージ、バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params) #下記paramsとセット
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all #バリデーションのために@インスタンス変数に変更
+    @book = Book.new(book_params) #下記paramsとセット #バリデーションのために@インスタンス変数に変更
+    if @book.save #バリデーションのために@インスタンス変数に変更
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id) #showへ #バリデーションのために@インスタンス変数に変更
+    else
+    render :index
+    end
   end
 
   def show
@@ -18,9 +23,20 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id]) #バリデーションのために@インスタンス変数に変更
+    if @book.update(book_params) #バリデーションのために@インスタンス変数に変更
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id) #showへ #バリデーションのために@インスタンス変数に変更
+    else
+    render :edit
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, presence: true #バリデーションで必須入力にする
+  validates :body, presence: true #バリデーションで必須入力にする
 end
